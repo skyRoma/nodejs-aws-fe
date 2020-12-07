d9bd5cb0bc3162298954340a0626b017ff6bcaed
Roman <roma.sluka.97@mail.ru>
2020-12-07 14:41:55 +0300

    Added proxy url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://00itcm6s0m.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://gztegs4kqe.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'http://skyroma-cart-api-dev.eu-west-1.elasticbeanstalk.com/api',
+  cart: 'https://sjxd5ukr8a.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
 export default API_PATHS;
